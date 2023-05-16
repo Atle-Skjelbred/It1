b8de3b16a335214bd84ce39a44939913e36951c3
Atle-Skjelbred <113422479+Atle-Skjelbred@users.noreply.github.com>
2023-05-16 10:07:22 +0200

    Add files via upload

---
+++ Kortdata.js
@@ -0,0 +1,9 @@
+let vissitkort = {
+  kortArray: [
+    { "navn": 'Atle Skjelbred',"stilling":"Elev","Dato":"2005-12-26","Sex":"Mann","Mobil":"48381518","Førerkort":"Ja" },
+    { "navn": 'Edvard Tobiasen',"stilling":"Elev","Dato":"2005-12-26","Sex":"Mann","Mobil":"48518","Førerkort":"Ja" },
+    { "navn": 'Andreas Guzman',"stilling":"Elev","Dato":"2005-12-26","Sex":"Mann","Mobil":"4831518","Førerkort":"Nei" },
+    { "navn": 'Eirik Djup',"stilling":"Elev","Dato":"2005-12-26","Sex":"Mann","Mobil":"48","Førerkort":"Nei" },
+    
+  ]
+}
